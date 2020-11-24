dc4faa0479490c324098845ca7ef4c31aca9ce7d
Agnieszka Wudzinska <wudzinska.agnieszka@gmail.com>
2020-11-24 02:30:33 +0100

    Dodanie nowej opłaty stałej do bazy

--- app/src/main/java/com/example/provistudent/NewCashActivity.java
+++ app/src/main/java/com/example/provistudent/NewCashActivity.java
@@ -2,56 +2,149 @@ package com.example.provistudent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewCashActivity extends AppCompatActivity {
     Button przyciskzapisz;
     Button przyciskcofnij;
     Button przyciskdodaj;
+    Button przyciskusun;
+    Button przyciskwyswietl;
+    Button przyciskedytuj;
+    Bazadanych bazadanych;
+    Cursor cursor;
+    TextView polekwota;
+    TextView poleoplata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_cash);
 
+        bazadanych = new Bazadanych(NewCashActivity.this);
+        cursor = bazadanych.odczytajtekst3();
+
+        poleoplata = findViewById(R.id.poleoplata);
+        polekwota = findViewById(R.id.polekwota);
         przyciskcofnij = (Button) findViewById(R.id.cofnij);
         przyciskcofnij.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view)
-            {
-                Intent intent = new Intent(NewCashActivity.this, RegisterActivity.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(NewCashActivity.this, CashActivity.class);
                 startActivity(intent);
             }
         });
 
-        przyciskdodaj = (Button) findViewById(R.id.dodajkolejne);
+        przyciskusun = (Button) findViewById(R.id.usun);
+        przyciskusun.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Integer usunwiersz = bazadanych.usuntekst3(polekwota.getText().toString());
+                if (usunwiersz > 0)
+                    Toast.makeText(getApplicationContext(), "Usunięto!", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(), "Nie można usunąć wiersza", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        przyciskedytuj = (Button) findViewById(R.id.edytuj);
+        przyciskedytuj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String kwota = polekwota.getText().toString();
+                String oplata = poleoplata.getText().toString();
+                if (cursor.getCount() == 0) {
+                    Toast.makeText(getApplicationContext(), "Nie można zaaktualizować!", Toast.LENGTH_SHORT).show();
+                } else if (cursor.getCount() > 0) {
+                    if (!kwota.isEmpty()) {
+                        if (bazadanych.zaaktualizujtekst3(oplata, kwota)) {
+                            polekwota.setText("");
+                            Toast.makeText(getApplicationContext(), "Dane zostały zaaktualizowane!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Nie zostały wprowadzone dane!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                cursor.close();
+            }
+        });
+
+        przyciskwyswietl = (Button) findViewById(R.id.wyswietl);
+        przyciskwyswietl.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                cursor = bazadanych.odczytajtekst3();
+                if (cursor.getCount() == 0) {
+                    wyswietlwiadomosc("Error", "Brak zapisanych zasobów!");
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (cursor.moveToNext()) {
+                    buffer.append("ID: " + cursor.getString(0) + "\n");
+                    buffer.append("Opłata: " + cursor.getString(1) + "\n");
+                    buffer.append("Kwota: " + cursor.getString(2) + "\n");
+                }
+                wyswietlwiadomosc("Zapisane wydatki stałe: ", buffer.toString());
+                cursor.close();
+            }
+        });
+
+        przyciskdodaj = (Button) findViewById(R.id.dodaj);
         przyciskdodaj.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 onDodaj();
             }
         });
 
         przyciskzapisz = (Button) findViewById(R.id.zapisz);
         przyciskzapisz.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 onZapisz();
             }
         });
     }
+
     //Metoda wykorzystywana podczas wywołania przycisku "Zapisz"
     void onZapisz() {
-        Intent intent = new Intent(NewCashActivity.this, MainActivity.class);
-        startActivity(intent);
-        //Metoda do zapisywania
+        if (cursor.getCount() == 0) {
+            Toast.makeText(getApplicationContext(), "Błąd! Nic nie zostało zapisane", Toast.LENGTH_SHORT).show();
+        } else if (cursor.getCount() > 0) {
+            Toast.makeText(getApplicationContext(), "Zapisano!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(NewCashActivity.this, CashActivity.class);
+            startActivity(intent);
+        }
+        cursor.close();
     }
-    //Metoda wykorzystywana podczas wywołania przycisku "Zapisz"
+
     void onDodaj() {
-        Intent intent = new Intent(NewCashActivity.this, MainActivity.class);
-        startActivity(intent);
-        //Metoda do dodawania
+        String kwota = polekwota.getText().toString();
+        String oplata = poleoplata.getText().toString();
+
+
+        if (!kwota.isEmpty()) {
+            if (bazadanych.dodajtekst3(oplata, kwota)) {
+                polekwota.setText("");
+                poleoplata.setText("");
+            }
+            Toast.makeText(getApplicationContext(), "Dodano!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Błąd! Nic nie zostało wprowadzone.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void wyswietlwiadomosc(String tytul, String wiadomosc) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(tytul);
+        builder.setMessage(wiadomosc);
+        builder.show();
     }
 }
